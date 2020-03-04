acf67e3b08ebd68ff837ee50bff12cabaef8eb79
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-03-04 16:04:18 +0100

    Dashboard

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Devices - Dashboard
+ *
+ * @package Coordinator\Modules\Devices
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ // redirect to devices list
+ api_redirect(api_url(["scr"=>"devices_list"]));
+?>
