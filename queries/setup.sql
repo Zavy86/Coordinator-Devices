b64868f645c2f6bfd8e0a614b0af885ade886aab
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-03-04 16:13:26 +0100

    Setup queries

---
+++ queries/setup.sql
@@ -0,0 +1,83 @@
+--
+-- Devices - Setup (1.0.0)
+--
+-- @package Coordinator\Modules\Devices
+-- @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+-- @link    http://www.coordinator.it
+--
+
+-- --------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices__devices`
+--
+
+CREATE TABLE IF NOT EXISTS `devices__devices` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `code` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `identifier` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `price` double UNSIGNED DEFAULT NULL,
+  `purchase` date DEFAULT NULL,
+  `warranty` date DEFAULT NULL,
+  `note` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices__devices__logs`
+--
+
+CREATE TABLE IF NOT EXISTS `devices__devices__logs` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `fkObject` int(11) UNSIGNED NOT NULL,
+  `fkUser` int(11) UNSIGNED DEFAULT NULL,
+  `timestamp` int(11) UNSIGNED NOT NULL,
+  `alert` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `event` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `properties_json` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`),
+  KEY `fkObject` (`fkObject`),
+  CONSTRAINT `devices__devices__logs_ibfk_1` FOREIGN KEY (`fkObject`) REFERENCES `devices__devices` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices__devices__join__documents`
+--
+
+CREATE TABLE IF NOT EXISTS `devices__devices__join__documents` (
+  `fkDevice` int(11) UNSIGNED NOT NULL,
+  `fkDocument` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`fkDevice`,`fkDocument`),
+  KEY `fkDevice` (`fkDevice`),
+  KEY `fkDocument` (`fkDocument`),
+  CONSTRAINT `devices__devices__join__documents_ibfk_1` FOREIGN KEY (`fkDevice`) REFERENCES `devices__devices` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `devices__devices__join__documents_ibfk_2` FOREIGN KEY (`fkDocument`) REFERENCES `archive__documents` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Authorizations
+--
+
+INSERT IGNORE INTO `framework__modules__authorizations` (`id`,`fkModule`,`order`) VALUES
+('devices-manage','devices',1),
+('devices-usage','devices',2);
+
+-- --------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- --------------------------------------------------------
